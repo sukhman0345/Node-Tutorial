32f9d445c825615f782806eb4818fdd6da45154e
Sukhman singh <sunnysaini062001@gmail.com>
2025-08-15 22:25:08 +0530

    Update app.js

--- Lecture 9_Express.js_starting/app.js
+++ Lecture 9_Express.js_starting/app.js
@@ -1,5 +1,5 @@
 // Core Module
-const http = require("http");
+// const http = require("http"); this are already defined in the express.js so need to make it
 
 // External Module
 const express = require('express');
@@ -12,19 +12,21 @@ const app = express();
 
 // Adding middleware 1st 
 //Note we can add multiple middleware
-app.use((req, res, next)=>{
+app.use('/',(req, res, next)=>{
  console.log("Came in first middleware", req.url, req.method);
+ console.log("<p>YOU ARE IN THE FIRST MIDDLEWARE </p>")
  next(); // Here we pass the request to the second middleware one its work is done
 });
 
 // Second middleware
-app.use((req, res, next)=>{
+app.use("/submit-detalis", (req, res, next)=>{
  console.log("Came in second middleware", req.url, req.method);
+ res.send("<p>YOU ARE IN THE SECOND MIDDLEWARE</p>")
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app);  this are already defined in the express.js so need to make it
 const PORT = 3000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => { // use app instead of the server
   console.log(`Server is listen at http://localhost${PORT}`);
 })

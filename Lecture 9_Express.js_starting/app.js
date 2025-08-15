ada34bc759dbb76c1eb6327795322b2674f438fb
Sukhman singh <sunnysaini062001@gmail.com>
2025-08-15 21:36:46 +0530

    Middleware work done

--- Lecture 9_Express.js_starting/app.js
+++ Lecture 9_Express.js_starting/app.js
@@ -5,9 +5,24 @@ const http = require("http");
 const express = require('express');
 
 // Local Module
-const userRequestHandler = require('/user');
+const userRequestHandler = require('./user');
 
-const server = http.createServer(userRequestHandler);
+// Now execute the express
+const app = express();
+
+// Adding middleware 1st 
+//Note we can add multiple middleware
+app.use((req, res, next)=>{
+ console.log("Came in first middleware", req.url, req.method);
+ next(); // Here we pass the request to the second middleware one its work is done
+});
+
+// Second middleware
+app.use((req, res, next)=>{
+ console.log("Came in second middleware", req.url, req.method);
+});
+
+const server = http.createServer(app);
 const PORT = 3000;
 
 server.listen(PORT, () => {

933beb03a3954581e0fdc6a5d671036668a8c136
Sukhman singh <sunnysaini062001@gmail.com>
2025-08-19 21:34:33 +0530

    added html files

--- Lecture 10.2_airbnb/routes/userRouter.js
+++ Lecture 10.2_airbnb/routes/userRouter.js
@@ -1,9 +1,11 @@
+//core module
 const express = require("express");
+// external module
+const path = require("path")
+
 const userRouter = express.Router();
 
 userRouter.get("/", (req, res, next)=>{
-  res.send(`<h1>Welcome to airbnb</h1>
-    <a href="/host/add-home"> Add Home></a>
-    `)
+  res.sendFile(path.join(__dirname, '../', 'views', 'home.html'))
 })
 module.exports = userRouter;
